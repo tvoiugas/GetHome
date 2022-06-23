dc693beca967fd06c209eb1f63e58df19b464d9b
tvoiugas <tvoiugas@gmail.com>
2022-06-24 03:27:41 +0600

    Улучшения навбара и поисковая строка

--- static/js/custom.js
+++ static/js/custom.js
@@ -4,3 +4,11 @@ const navUL = document.getElementById("nav-ul");
 hamburger.addEventListener('click', () => {
 	navUL.classList.toggle('show');
 });
+
+const navLinks = document.querySelectorAll('.navbar-link');
+navLinks.forEach(function (navLink) {
+	navLink.addEventListener('click', function () {
+		navLinks.forEach(element => element.classList.remove('active'))
+		navLink.classList.add('active');
+	});
+});
