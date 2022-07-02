589781a5a6326cfd72845edf24d3d49a566cae7c
tvoiugas <tvoiugas@gmail.com>
2022-07-03 00:13:31 +0600

    Сделал навбар

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,14 +1,26 @@
-const hamburger = document.getElementById('hamburger-btn');
-const navUL = document.getElementById("nav-ul");
+const navUL = document.getElementById("nav-links");
+const navLinks = document.querySelectorAll('.nav-link');
 
-hamburger.addEventListener('click', () => {
-	navUL.classList.toggle('show');
-});
+const firstNav = document.getElementById("first-navbar");
+const secondNav = document.getElementById("second-navbar");
 
-const navLinks = document.querySelectorAll('.navbar-link');
+
 navLinks.forEach(function (navLink) {
 	navLink.addEventListener('click', function () {
 		navLinks.forEach(element => element.classList.remove('active'))
 		navLink.classList.add('active');
 	});
+});
+
+let lastScrollY = window.scrollY;
+
+window.addEventListener('scroll', () => {
+	if (lastScrollY < window.scrollY) {
+		firstNav.style.top = "-60px";
+		secondNav.style.top = "0";
+	} else {
+		firstNav.style.top = "0px";
+		secondNav.style.top = "60px";
+	};
+
 });
