80da3ff7011cb5729d158f1387dabc068a4ade6a
Daniil <MystiDanny@gmail.com>
2022-07-29 19:24:01 +0600

    создал декоратор который проверяет автора объявления для редактирования

--- estates/views.py
+++ estates/views.py
@@ -6,7 +6,8 @@ from django.urls import reverse
 from .forms import EstateForm, DetailsForm, FeaturesForm, FeaturesFormSet
 from .models import Estate, Details, Feature
 from .filters import EstateFilter
-
+from .decorators import check_author
+from django.contrib import messages
 
 class HTTPResponseHXRedirect(HttpResponseRedirect):
     def __init__(self, *args, **kwargs):
@@ -164,6 +165,7 @@ def estate_delete(request, listing_id):
 
 
 @login_required
+@check_author
 def estate_update(request, listing_id):
     estate = get_object_or_404(Estate, id=listing_id)
     form = EstateForm(request.POST or None, instance=estate)
@@ -171,9 +173,10 @@ def estate_update(request, listing_id):
         if form.is_valid:
             form.save()
             return redirect('listing_list')
-
+    
     context = {
         'form': form
     }
 
     return render(request, 'estates/estate_update.html', context)
+
