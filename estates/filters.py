bae10b5106ad75aa8836cedc039569295f98f17a
Aleksei <smaut.555.kg@gmail.com>
2022-08-06 04:01:05 +0600

    delete,images,detail,slides

--- estates/filters.py
+++ estates/filters.py
@@ -60,8 +60,8 @@ TOWNS_CHOICES_FILTER = [
 class EstateFilter(django_filters.FilterSet):
     tags = django_filters.ModelMultipleChoiceFilter(
         label='тег', field_name="tags", queryset=Tag.objects.all(), widget=django_filters.widgets.LinkWidget)
-    estate_type = django_filters.ChoiceFilter(choices=Estate.ESTATE_TYPE_CHOICES)
-    location = django_filters.ChoiceFilter(choices=Estate.TOWNS_CHOICES)
+    estate_type = django_filters.ChoiceFilter(choices=Estate.ESTATE_TYPE_CHOICES, empty_label='estate')
+    location = django_filters.ChoiceFilter(choices=Estate.TOWNS_CHOICES, empty_label='Uncategorized')
     bedrooms = django_filters.NumberFilter(field_name='details__bedrooms')
     
     class Meta:
