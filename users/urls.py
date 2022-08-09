2184e941d937dd89734bea6f4fd128808243fab6
Aleksei <smaut.555.kg@gmail.com>
2022-08-09 16:26:31 +0600

    profile, delete(templates)

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import (
-    login_page, logout_page, register_page,
+    login_page, logout_page, register_page, profile_page,
      account_activation)
 
 
@@ -8,6 +8,6 @@ urlpatterns = [
     path('login_page', login_page, name='login_page'),
     path('logout_page', logout_page, name='logout_page'),
     path('register_page', register_page, name='register_page'),
-    # path('profile', profile_page, name='profile'),
+    path('profile', profile_page, name='profile'),
     path('activate/<uidb64>/<token>', account_activation, name='activation'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,9 +1,11 @@
 import re
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.sites.shortcuts import get_current_site
 from django.utils.encoding import force_bytes, force_str
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.template.loader import render_to_string
+
+from estates.models import Estate
 from .tokens import account_activation_token
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.admin.forms import AuthenticationForm
@@ -12,7 +14,7 @@ from django.contrib.auth.decorators import login_required
 
 from .models import CustomUser
 from .forms import CustomUserCreationForm
-# , ProfileForm
+
 
 
 
@@ -49,36 +51,6 @@ def register_page(request):
 		'form': form
 	}
 	return render(request, 'users/register_page.html', context)
-	# if request.user.is_authenticated:
-	# 	return redirect('index')
-	# form = CustomUserCreationForm()
-	# form_profile = ProfileForm()
-	# if request.method == 'POST':
-	# 	form = CustomUserCreationForm(request.POST)
-	# 	form_profile = ProfileForm(request.POST)
-	# 	if form.is_valid() and form_profile.is_valid():
-	# 		user = form.save(commit=False)
-	# 		user.is_active = False
-	# 		user.save()
-
-	# 		profile = form_profile.save(commit=False)
-	# 		profile.user = user
-	# 		profile.save()
-
-	# 		current_site = get_current_site(request)
-	# 		subject = 'Активация'
-	# 		message = render_to_string('users/activation_page.html', {
-	# 			'user': user, 'domain': current_site.domain,
-	# 			'uid': urlsafe_base64_encode(force_bytes(user.pk)),
-	# 			'token': account_activation_token.make_token(user)
-	# 		})
-	# 		user.email_user(subject=subject, message=message)
-	# 		return redirect('index')
-	# context = {
-	# 	'form': form,
-	# 	'profile': form_profile
-	# }
-	# return render(request, 'users/register_page.html', context)
 
 def account_activation(request, uidb64, token):
 	try:
@@ -95,7 +67,9 @@ def account_activation(request, uidb64, token):
 	else:
 		return render(request, 'users/activation_failed.html')
 
-# @login_required
-# def profile_page(request):
-# 	profile = CustomUser.objects.get(user= request.user)
-# 	return render(request, 'users/profile.html', {'profile': profile})
+@login_required
+def profile_page(request):
+    # profile = get_object_or_404(CustomUser)
+    estete = Estate.objects.all().filter(author=request.user)
+   
+    return render(request, 'users/profile.html', {'estate': estete})
